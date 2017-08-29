7defb11e93d353a1e688b41f3932e2acb18d7936
Brennen Bull <brennenbull@gmail.com>
2017-08-29 13:20:57 -0600

    new note route

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 router.get('/?', (req, res, next)=>{
-  console.log(req.query)
   let users_id = 1;
   let hostname = req.query.host;
   knex('host')
@@ -35,19 +34,87 @@ router.get('/?', (req, res, next)=>{
 })
 router.get('/:id', (req, res, next)=>{
   let id = req.params.id;
-  console.log(id);
+
 })
 router.post('/:host', (req, res, next)=>{
   let host = req.params.host;
-  console.log(host);
-  res.send({
-    "message":"In post",
-    "userNotes":"true",
-    "notes":{
-      "title":`${req.body.title}`,
-      "content":`${req.body.content}`
-    }
-  });
+  let users_id = 1;
+  let postObj = {
+    "title":req.body.title,
+    "content":req.body.content,
+    "users_id":users_id
+  }
+  knex('notes')
+    .insert(postObj, "*")
+    .then((newNote)=>{
+      let newNoteId = newNote[0].id;
+      console.log(newNote);
+      knex('host')
+        .select("*")
+        .where('hostname', host)
+        .then((hostObj)=>{
+          if(hostObj.length === 0){
+            knex('host')
+              .insert({hostname:host}, "*")
+              .then((newhost)=>{
+                let hostId = newhost[0].id;
+                  knex('notes_host')
+                    .insert({'host_id':hostId,'notes_id':newNoteId},'*')
+                    .then((hostClue)=>{
+                      knex('notes_host')
+                      .select('*')
+                      .where('host_id', hostClue[0].host_id)
+                      .join('notes', 'notes_host.notes_id', '=', 'notes.id')
+                      .then((note)=>{
+                        let notesArr = [];
+                        note.forEach((ele, i)=>{
+                          notesArr.push({
+                            'title':ele.title,
+                            'content':ele.content
+                          });
+                        })
+                        res.send({
+                          "userNotes":"true",
+                          "notes":notesArr
+                        })
+                    })
+                  })
+              })
+          }else{
+            let hostId = hostObj[0].id;
+              knex('notes_host')
+                .insert({'host_id':hostId,'notes_id':newNoteId},'*')
+                .then((hostClue)=>{
+                  knex('notes_host')
+                  .select('*')
+                  .where('host_id', hostClue[0].host_id)
+                  .join('notes', 'notes_host.notes_id', '=', 'notes.id')
+                  .then((note)=>{
+                    let notesArr = [];
+                    note.forEach((ele, i)=>{
+                      notesArr.push({
+                        'title':ele.title,
+                        'content':ele.content
+                      });
+                    })
+                    res.send({
+                      "userNotes":"true",
+                      "notes":notesArr
+                    })
+                })
+              })
+          }
+        })
+    })
+
+  // res.send({
+  //   "message":"In post",
+  //   "userNotes":"true",
+  //   "notes":{
+  //     "title":`${req.body.title}`,
+  //     "content":`${req.body.content}`
+  //   }
+  // });
 })
 router.patch('/', (req, res, next)=>{
   res.send({
